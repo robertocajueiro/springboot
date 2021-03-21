182ddc7c0e60a62110ff9678bd66125a8336bf39
unknown <beto.cajueiro@gmail.com>
2021-03-21 04:02:52 -0300

    Spring boot @Configuration @Bean

---
+++ src/main/java/io/github/robertocajueiro/MinhaConfiguration.java
@@ -0,0 +1,15 @@
+package io.github.robertocajueiro;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "Sistema de vendas";
+
+    }
+}
