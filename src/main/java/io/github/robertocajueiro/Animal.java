3fc22c1d94d3d1582c584303e44f44efc48d8832
unknown <beto.cajueiro@gmail.com>
2021-03-22 01:17:08 -0300

    Criando annotations customizadas de configuração

---
+++ src/main/java/io/github/robertocajueiro/Animal.java
@@ -0,0 +1,5 @@
+package io.github.robertocajueiro;
+
+public interface Animal {
+    void fazerBarulho();
+}
--- src/main/java/io/github/robertocajueiro/MinhaConfiguration.java
+++ src/main/java/io/github/robertocajueiro/MinhaConfiguration.java
@@ -3,11 +3,8 @@ package io.github.robertocajueiro;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+@Development
 public class MinhaConfiguration {
 
     @Bean
