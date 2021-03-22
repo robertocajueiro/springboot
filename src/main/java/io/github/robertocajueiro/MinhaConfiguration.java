06f57321affef97df470dcefed8262519f4ccc18
unknown <beto.cajueiro@gmail.com>
2021-03-22 00:51:02 -0300

    Trabalhando com múltiplos ambientes e profiles

--- src/main/java/io/github/robertocajueiro/MinhaConfiguration.java
+++ src/main/java/io/github/robertocajueiro/MinhaConfiguration.java
@@ -1,15 +1,20 @@
 package io.github.robertocajueiro;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sistema de Vendas";
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+          System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
 
     }
 
